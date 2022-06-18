89776645cf780aa38759f310a73ec20151cf191a
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2022-06-18 14:39:17 +0530

    changes made

--- app.js
+++ app.js
@@ -31,7 +31,16 @@ app.use(xss());
 app.use(cors());
 app.use(helmet());
 
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // routes
+app.get("/", (req, res) => {
+  res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 
